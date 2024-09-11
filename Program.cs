02d620d00fbd2300e93bd0922389d62a2b5ad8a3
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-11 14:10:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Temperatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? answer;
+            int temp;
+            bool isNumber;
+            
+            do {
+                Console.WriteLine("Ange en temperatur i grader celsius");
+                
+                answer = Console.ReadLine();
+                isNumber = int.TryParse(answer, out temp);
+                
+                if (isNumber) {
+                    if (temp >= 30) {
+                        Console.WriteLine("Jättevarmt", Console.ForegroundColor = ConsoleColor.Red);
+                    }
+                    else if (temp >= 20 && temp <= 30) {
+                        Console.WriteLine("Varmt", Console.ForegroundColor = ConsoleColor.Yellow);
+                    }
+                    else {
+                        Console.WriteLine("Hösttemperatur", Console.ForegroundColor = ConsoleColor.Blue);
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                
+            } while (isNumber);
+        }
+    }
+}
